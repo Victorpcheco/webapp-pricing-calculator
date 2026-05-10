3b4f1b2b87d2a479b20f118798960efa64eca2f5
Victor Pacheco <victorpachecodearruda@gmail.com>
2026-05-09 22:58:27 -0300

    Configurando banco de dados na aplicação

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,6 +1,13 @@
+using Infrastructure.DI.Shared;
+using Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AutoInjectAll();
+// Carrega as variáveis do .env subindo na árvore de diretórios
+DotNetEnv.Env.TraversePath().Load();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AutoInjectAll();
 
 var app = builder.Build();
 
