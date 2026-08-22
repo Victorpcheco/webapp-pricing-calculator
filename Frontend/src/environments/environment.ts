eb307f6c05b7781617fe6c193a036cb7d1121a6e
Victor Pacheco <victorpachecodearruda@gmail.com>
2026-08-22 18:25:13 -0300

    Feat: adicionando url da api de producao

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://minha-api-em-producao.onrender.com' // <-- Substitua pela URL do Render depois!
+  apiUrl: 'https://bff-webapp-pricing-calculator.onrender.com'
 };
